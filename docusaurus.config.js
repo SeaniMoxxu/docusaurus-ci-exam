9b53b70095e5e986ce4a6407e824cb1d4ee481fa
Coillard Thomas <tcoillard@myges.fr>
2024-07-24 16:27:59 +0200

    🚀 Ajout de la configuration CI pour le déploiment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,15 +8,15 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Le meilleur docusaurus',
+  tagline: 'Dinosaurs are awesome and very very cool !!!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://SeaniMoxxu.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-ci-exam',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
